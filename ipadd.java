e0294381389843cae861573d634e663fc9f26fcb
benitaofficial16-wq <benitaofficial16@gmail.com>
2026-07-16 11:07:14 +0530

    Add files via upload

---
+++ ipadd.java
@@ -0,0 +1,5 @@
+class ipadd {
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+}
---
+++ pallindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int temp = x;
+        int sum = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            sum = sum * 10 + digit;
+            x = x / 10;
+        }
+
+        return temp == sum;
+    }
+}
